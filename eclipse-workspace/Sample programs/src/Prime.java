90457b0305b06b0f55404819ffe0119acd65ca2d
hp <hp@DESKTOP-LD0RQ0O>
2018-05-03 14:15:23 +0530

    final commit

--- eclipse-workspace/Sample programs/src/Prime.java
+++ eclipse-workspace/Sample programs/src/Prime.java
@@ -2,30 +2,30 @@
 class Prime // program to print 10001 prime number 
 {
 	 
-public static void main(String args[])
-{
-int number=3,count=1;// first prime number is 2. so checking starts from 3 and count is set to 1
-while(count<10001)
-{
-	if(checkprime(number)==1) // function to check whether a number is prime.
+	public static void main(String args[])
+	{
+		int number=3,count=1;// first prime number is 2. so checking starts from 3 and count is set to 1
+			while(count<10001)
+				{
+					if(checkprime(number)) // function to check whether a number is prime.
 	
-		count++;
-	if(count==10001)
-		System.out.println("The 10001 prime number  is :"+number);
-	number++;
-}
-}
+						count++;
+					if(count==10001)
+						System.out.println("The 10001 prime number  is :"+number);
+					number++;
+				}
+	}
 
-public  static int checkprime(int n)//  method to check whether a number is prime. Returns 1 if prime
-{
+	public  static boolean checkprime(int n)//  method to check whether a number is prime. Returns 1 if prime
+	{
 	
-for(int k=2;k<=Math.sqrt(n);k++)// sufficient to check till the square root of the number
-{
-	if(n%k==0)
-	 return 0;// the number is composite
+			for(int k=2;k<=Math.sqrt(n);k++)// sufficient to check till the square root of the number
+			{
+				if(n%k==0)
+					return false;// the number is composite
 	
-}
-return 1;// the number is prime if there are no divisors
-}
+			}
+			return true;// the number is prime if there are no divisors
+	}
 
 }
--- eclipse-workspace/Sample programs/src/Pythogorus.java
+++ eclipse-workspace/Sample programs/src/Pythogorus.java
@@ -5,9 +5,9 @@
 	 public static void main(String args[])
 	 {
 		int c;
-		 for(int a=1;a<=1000/3;a++)        // a+b+c=1000 .Therefore,assuming  a<b .Then a should be <= 1000/3
+		for(int a=1;a<=1000/3;a++)        // a+b+c=1000 .Therefore,assuming  a<b .Then a should be <= 1000/3
 		
-			 for(int b=a+1;b<=1000/2;b++) // looping through possibilities of b
+	     for(int b=a+1;b<=1000/2;b++) // looping through possibilities of b
 			 {
 				 c=1000-a-b;               //Assigning value to c
 						 if(a*a + b*b == c*c)
